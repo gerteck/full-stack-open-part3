88e57198be0b4cd53185eb300ba139ba573b3dea
Ger Teck <changerteck@gmail.com>
2024-07-11 15:59:30 +0800

    Add Ex3.2/3/4

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # Phonebook Backend
 
 Node application.
-
 Nodejs, Express
+
+#### Running Server Locally
+
+* `npm run dev` to run server on localhost port 3001.
+
 
 #### Dependencies:
 * Express (Web framework for Node.js)
 * Nodemon (Auto restart node application)
+
+Install dependencies: `npm install`
+
+### Extensions
+* VS Code: REST client plugin
+  * `/requests/*.rest`
+* (or use Postman)
--- index.js
+++ index.js
@@ -25,15 +25,38 @@ let persons = [
 ];
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>');
-})
+    response.send('<h1>API for Phonebook</h1>');
+});
+
+app.get('/info', (request, response) => {
+    const date = new Date();
+    response.send(`<p>Phonebook has info for ${persons.length} people</p> <br/> <p>DateTime: ${date}</p>`);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const person = persons.find(person => person.id === id);
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    persons = persons.filter(person => person.id !== id);
+
+    // Does not delete permanently for now.
+    response.status(204).end();
+});
 
 app.get('/api/persons', (request, response) => {
     response.json(persons);
-})
+});
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     console.log(`http://localhost:${PORT}`)
-})
+});
